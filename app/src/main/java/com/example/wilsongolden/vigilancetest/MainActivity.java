f0da0237f26f5233aa31a97cbb30c9a84a45fe72
goldenguy00 <goldenguy00@gmail.com>
2016-11-26 13:47:49 -0500

    Version 1.0 Set up background processing

--- app/src/main/java/com/example/wilsongolden/vigilancetest/MainActivity.java
+++ app/src/main/java/com/example/wilsongolden/vigilancetest/MainActivity.java
@@ -1,40 +1,65 @@
 package com.example.wilsongolden.vigilancetest;
 
-import android.support.v7.app.ActionBar;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
     Button myButton;
-    MyThread thread;
+    TextView hit, miss, falseStart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        thread = new MyThread();
 
-        thread.start();
-        myButton = new Button(this);
-        myButton.setText("Push Me");
-        myButton.setBackgroundColor(getColor(R.color.black));
+        myButton = (Button) findViewById(R.id.button);
         myButton.setOnClickListener(new buttonListener());
-
-        RelativeLayout mainLayout = (RelativeLayout)findViewById(R.id.activity_main);
-        ActionBar.LayoutParams layoutParams = new ActionBar.LayoutParams(ActionBar.LayoutParams.WRAP_CONTENT, ActionBar.LayoutParams.WRAP_CONTENT);
-        mainLayout.addView(myButton, layoutParams);
+        hit = (TextView) findViewById(R.id.hits);
+        miss = (TextView) findViewById(R.id.misses);
+        falseStart = (TextView) findViewById(R.id.falseStarts);
     }
 
     private class buttonListener implements View.OnClickListener {
         public void onClick(View view) {
-            myButton.setBackgroundColor(getColor(R.color.white));
-            thread.endThread();
+            myButton.setBackgroundColor(getColor(R.color.black));
+            new waitTask().execute();
+        }
+    }
+
+    private class waitTask extends AsyncTask<Void, Void, Integer> {
+
+        @Override
+        protected void onPreExecute() {
+
+        }
+
+        @Override
+        protected Integer doInBackground(Void... voids) {
+            Random rand = new Random();
+            int random = 1 + rand.nextInt(9);
+            System.out.println(random);
+
+            try {
+                Thread.sleep(random * 1000);
+            } catch (Exception e) {
+                System.out.println("False Start");
+            }
+
+            System.out.println("White");
+            return getColor(R.color.white);
+        }
+
+        @Override
+        protected void onPostExecute(Integer result) {
+            myButton.setBackgroundColor(result);
+            System.out.println(result);
         }
     }
 }
